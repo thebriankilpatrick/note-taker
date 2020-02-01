1f7a7ac698021c5834d92717b74c3064cc3baf43
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-02-01 15:35:15 -0500

    Added script start, and process.env.port

--- Develop/server.js
+++ Develop/server.js
@@ -6,7 +6,7 @@ const util = require("util");
 const notes = require("../Develop/db/db.json");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
